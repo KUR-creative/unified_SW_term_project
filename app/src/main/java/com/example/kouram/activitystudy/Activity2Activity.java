1bc35902351f96b1d22fa70cc877825ad1e6f5ef
KUR <rhdnfka94@gmail.com>
2017-06-16 11:00:03 +0900

    now it can add marker.

--- app/src/main/java/com/example/kouram/activitystudy/Activity2Activity.java
+++ app/src/main/java/com/example/kouram/activitystudy/Activity2Activity.java
@@ -1,14 +1,20 @@
 package com.example.kouram.activitystudy;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
 import com.skp.Tmap.TMapView;
 
 public class Activity2Activity extends AppCompatActivity {
 
+    private TMapView mapView;
     private Button addMarkerBtn;
     private Button routeBtn;
 
@@ -17,22 +23,24 @@ public class Activity2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
 
-        TMapView tmapview = (TMapView)findViewById(R.id.map_view);
-        tmapview.setSKPMapApiKey("9553cc22-8104-3088-a882-b90ef2a051d7");
-        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+        mapView = (TMapView)findViewById(R.id.map_view);
+        mapView.setSKPMapApiKey("9553cc22-8104-3088-a882-b90ef2a051d7");
+        mapView.setLanguage(TMapView.LANGUAGE_KOREAN);
 
-        tmapview.setIconVisibility(true);
-        tmapview.setZoomLevel(17);
-        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
-        tmapview.setCompassMode(false);
-        tmapview.setTrackingMode(true);
-        tmapview.setSightVisible(true);
+        mapView.setIconVisibility(true);
+        mapView.setZoomLevel(17);
+        mapView.setMapType(TMapView.MAPTYPE_STANDARD);
+        mapView.setCompassMode(false);
+        mapView.setTrackingMode(true);
+        mapView.setSightVisible(true);
 
         addMarkerBtn = (Button)findViewById(R.id.add_marker_btn);
         addMarkerBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 System.out.println("add-marker");
+                TMapPoint point = mapView.getCenterPoint();
+                addMarker(point.getLatitude(), point.getLongitude(), "My Marker");
             }
         });
 
@@ -43,5 +51,23 @@ public class Activity2Activity extends AppCompatActivity {
                 System.out.println("get-route");
             }
         });
+    }
+
+    private int id = 0;
+    private void addMarker(double lat, double lng, String title) {
+        TMapMarkerItem item = new TMapMarkerItem();
+        TMapPoint point = new TMapPoint(lat, lng);
+        item.setTMapPoint(point);
+
+        // just '+' marker!
+        Bitmap icon = ((BitmapDrawable) ContextCompat.getDrawable(this, android.R.drawable.ic_input_add)).getBitmap();
+        item.setIcon(icon);
+        item.setPosition(0.5f, 1);
+        item.setCalloutTitle(title);
+        item.setCalloutSubTitle("sub " + title);
+
+        mapView.addMarkerItem("m" + id, item);
+        id++;
+        // 이걸로 삭제 가능. if(id == 2) mapView.removeMarkerItem("m1");
     }
 }
