9a20c2bc61ff4829335cd6f875eef4b6326eb97c
KUR <rhdnfka94@gmail.com>
2017-06-24 22:53:25 +0900

    adit popupActivity

--- app/src/main/java/com/example/kouram/activitystudy/PopUpActivity.java
+++ app/src/main/java/com/example/kouram/activitystudy/PopUpActivity.java
@@ -1,13 +1,49 @@
 package com.example.kouram.activitystudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PopUpActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        WindowManager.LayoutParams layoutParams= new WindowManager.LayoutParams();
+        layoutParams.flags= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount= 0.7f;
+        getWindow().setAttributes(layoutParams);
+
         setContentView(R.layout.activity_pop_up);
     }
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction() == MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+
 }
