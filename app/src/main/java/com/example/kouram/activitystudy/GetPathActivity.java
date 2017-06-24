7d7eed6730a3e7ecf1b8c582d5ea0c0c41c92212
KUR <rhdnfka94@gmail.com>
2017-06-24 22:58:02 +0900

    add Button clickListener in getPathActivity

--- app/src/main/java/com/example/kouram/activitystudy/GetPathActivity.java
+++ app/src/main/java/com/example/kouram/activitystudy/GetPathActivity.java
@@ -1,7 +1,10 @@
 package com.example.kouram.activitystudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetPathActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class GetPathActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_path);
+
+        Button setRadiusBtn = (Button)findViewById(R.id.set_radius_btn);
+        setRadiusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetPathActivity.this, PopUpActivity.class);
+                intent.putExtra("data", "Test Popup");
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 }
