cc3f94246a2b633353f6616ecc277b8cfd9b9edd
KUR <rhdnfka94@gmail.com>
2017-06-24 23:26:50 +0900

    now popup activity act!

--- app/src/main/java/com/example/kouram/activitystudy/PopUpActivity.java
+++ app/src/main/java/com/example/kouram/activitystudy/PopUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.kouram.activitystudy;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class PopUpActivity extends AppCompatActivity {
+public class PopUpActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +22,7 @@ public class PopUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pop_up);
     }
     //확인 버튼 클릭
-    public void mOnClose(View v){
+    public void onClose(View v){
         //데이터 전달하기
         Intent intent = new Intent();
         intent.putExtra("result", "Close Popup");
